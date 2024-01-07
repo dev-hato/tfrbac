578a81c6cb704b2d4a54e29bb17a1ef5c2b318fc
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2024-01-08 00:47:08 +0900

    add files

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+)
+
+func main() {
+	const terraformDir = "./" //TODO: あとで引数で弄れるようにしたい
+
+	err := filepath.Walk(terraformDir, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// '.tf' 拡張子を持つファイルのみをリストアップ
+		if strings.HasSuffix(info.Name(), ".tf") {
+			src, err := os.ReadFile(filePath)
+			if err != nil {
+				return err
+			}
+
+			file, diags := hclwrite.ParseConfig(src, filePath, hcl.InitialPos)
+			if diags.HasErrors() {
+				return err
+			}
+
+			body := file.Body()
+			for _, v := range body.Blocks() {
+				if slices.Contains([]string{"moved", "import"}, v.Type()) {
+					body.RemoveBlock(v)
+				}
+			}
+			if err = os.WriteFile(filePath, file.Bytes(), info.Mode()); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking through Terraform directory: %v\n", err)
+	}
+}

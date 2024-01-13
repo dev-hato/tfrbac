4a6a56db84fcd92d9d2ba1dfeeaf62657c8a1129
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2024-01-14 06:06:27 +0900

    nestを浅くする

--- main.go
+++ main.go
@@ -19,27 +19,29 @@ func main() {
 			return err
 		}
 
-		// '.tf' 拡張子を持つファイルのみをリストアップ
-		if strings.HasSuffix(info.Name(), ".tf") {
-			src, err := os.ReadFile(filePath)
-			if err != nil {
-				return err
-			}
+		// '.tf' 拡張子でなければスキップ
+		if !strings.HasSuffix(info.Name(), ".tf") {
+			return nil
+		}
 
-			file, diags := hclwrite.ParseConfig(src, filePath, hcl.InitialPos)
-			if diags.HasErrors() {
-				return err
-			}
+		src, err := os.ReadFile(filePath)
+		if err != nil {
+			return err
+		}
 
-			body := file.Body()
-			for _, v := range body.Blocks() {
-				if slices.Contains([]string{"moved", "import"}, v.Type()) {
-					body.RemoveBlock(v)
-				}
+		file, diags := hclwrite.ParseConfig(src, filePath, hcl.InitialPos)
+		if diags.HasErrors() {
+			return err
+		}
+
+		body := file.Body()
+		for _, v := range body.Blocks() {
+			if slices.Contains([]string{"moved", "import"}, v.Type()) {
+				body.RemoveBlock(v)
 			}
-			if err = os.WriteFile(filePath, file.Bytes(), info.Mode()); err != nil {
-				return err
-			}
+		}
+		if err = os.WriteFile(filePath, file.Bytes(), info.Mode()); err != nil {
+			return err
 		}
 
 		return nil

a222bb46f9f27d16371d6943ff6b4da8f0efb683
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-01-20 18:09:51 +0900

    fail-notify削除 (#39)

--- scripts/fix_fail_notify/fix_fail_notify/fix_fail_notify.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-workflows_dir=.github/workflows
-fail_notify_workflow_file="${workflows_dir}/fail-notify.yml"
-workflows="$(find "${workflows_dir}" -not -path "${fail_notify_workflow_file}" -type f -exec yq '.name' {} \; | sort | sed -e 's/^\(.*\)$/"\1"/g' | tr '\n' ',' | sed -e 's/,$//g')"
-yq -i ".on.workflow_run.workflows=[${workflows}]" "${fail_notify_workflow_file}"

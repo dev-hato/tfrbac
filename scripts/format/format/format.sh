6dc2698e25209e0cfe758b17817c0940c8bac969
Masaya Suzuki <suzukimasaya428@gmail.com>
2025-02-28 05:49:47 +0900

    fix

---
+++ scripts/format/format/format.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+go mod tidy
+tag_name="$(yq '.jobs.super-linter.steps[-1].uses' .github/workflows/super-linter.yml | sed -e 's;/slim@.*;:slim;g')"
+tag_version="$(yq '.jobs.super-linter.steps[-1].uses | line_comment' .github/workflows/super-linter.yml)"
+version="$(docker run --rm --entrypoint '' "ghcr.io/${tag_name}-${tag_version}" /bin/sh -c 'goreleaser -v' | grep GitVersion | sed -e 's/GitVersion: *//g')"
+yq -i ".jobs.goreleaser.steps[-1].with.version|=\"v$version\"" .github/workflows/release.yml

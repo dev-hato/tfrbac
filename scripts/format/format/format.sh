175d80aabfac2072db02985e1179475058d305b8
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2026-09-19 13:33:10 +0900

    ci: 整形CIでgo fixとgo fmtを実行

--- scripts/format/format/format.sh
+++ scripts/format/format/format.sh
@@ -2,6 +2,8 @@
 set -e
 
 go mod tidy
+go fix ./...
+go fmt ./...
 tag_name="$(yq '.jobs.super-linter.steps[-1].uses' .github/workflows/super-linter.yml | sed -e 's;/slim@.*;:slim;g')"
 tag_version="$(yq '.jobs.super-linter.steps[-1].uses | line_comment' .github/workflows/super-linter.yml)"
 version="$(docker run --rm --entrypoint '' "ghcr.io/${tag_name}-${tag_version}" /bin/sh -c 'goreleaser -v' | grep GitVersion | sed -e 's/GitVersion: *//g')"
